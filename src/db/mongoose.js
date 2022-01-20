4ada7982245cbc121d4f5fbc91102012e857bd90
DanielShamir93 <daniel.shamir.93@gmail.com>
2022-01-20 23:09:24 +0200

    Fix deploy problem take 3

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const DB_NAME = "Bank_API";
-
 const MONGOOSE_URL = `mongodb+srv://danielshamir93:4sXk3nH6OKl5tG35@danieldb.rhwdk.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
 
 mongoose.connect(
