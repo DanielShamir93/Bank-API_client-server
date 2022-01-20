3bf843182dfe032682d54e4095f05c6db6580bb9
DanielShamir93 <daniel.shamir.93@gmail.com>
2022-01-20 23:36:09 +0200

    Fix deploy problem take 4

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = express();
 const usersRoutes = require("./routes/users/users-routes");
 require("./db/mongoose");
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 const publicPath = path.join(__dirname, 'client/build');
 app.use(express.static(publicPath));
