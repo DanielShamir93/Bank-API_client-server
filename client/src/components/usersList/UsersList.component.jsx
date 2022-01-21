99c2e62832a0f278dd89d7c2de5cc104a5bcb442
DanielShamir93 <daniel.shamir.93@gmail.com>
2022-01-22 01:01:22 +0200

    Add search field to UserList component for filtering accounts

--- client/src/components/usersList/UsersList.component.jsx
+++ client/src/components/usersList/UsersList.component.jsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react';
-import myApi from '../../api/Api';
-import User from '../user/User.component';
-import './usersList.styles.scss';
+import { useState, useEffect } from "react";
+import { FcPlus, FcSearch } from "react-icons/fc";
+import myApi from "../../api/Api";
+import User from "../user/User.component";
+import "./usersList.styles.scss";
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -10,37 +11,41 @@ function App() {
   useEffect(() => {
     const getUsers = async () => {
       try {
-        const { data } = await myApi.get('/all');
+        const { data } = await myApi.get("/all");
         setUsers(data);
       } catch (err) {
         console.log(err.message);
       }
-    }
+    };
     getUsers();
   }, [isUserChanged]);
 
   const renderUsers = () => {
     return users.map((user) => {
       return (
-        <User 
-          key={user._id} 
-          user={user} 
-          isUserChanged={isUserChanged} 
+        <User
+          key={user._id}
+          user={user}
+          isUserChanged={isUserChanged}
           setIsUserChanged={setIsUserChanged}
         />
       );
     });
-  }
+  };
 
   return (
     <div className="Users-list">
-      <div className="gallery">
-        { renderUsers() }
+      <div className="search-account">
+        <input
+          className="search-account-input"
+          type="text"
+          placeholder="Search"
+        />
+        <FcSearch className="search-account-react-icon" />
       </div>
+      <div className="gallery">{renderUsers()}</div>
     </div>
   );
-
-
 }
 
 export default App;
